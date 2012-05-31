2c24666fede91f59b99a5147bdc8499c74100fc0
Sebastian <sebastian.sebald@gmail.com>
2012-05-31 10:21:13 +0200

    simulationEngine satisfies delegation, sod, bod and coi policies

--- src/de/freiburg/uni/iig/sisi/simulation/SimulationEngine.java
+++ src/de/freiburg/uni/iig/sisi/simulation/SimulationEngine.java
@@ -2,7 +2,6 @@ package de.freiburg.uni.iig.sisi.simulation;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Random;
 
 import de.freiburg.uni.iig.sisi.model.NarratorObject;
@@ -11,82 +10,91 @@ import de.freiburg.uni.iig.sisi.model.net.Place;
 import de.freiburg.uni.iig.sisi.model.net.Transition;
 import de.freiburg.uni.iig.sisi.model.resource.Role;
 import de.freiburg.uni.iig.sisi.model.resource.Subject;
+import de.freiburg.uni.iig.sisi.model.safetyrequirements.Policy;
+import de.freiburg.uni.iig.sisi.model.safetyrequirements.Policy.PolicyType;
 
 public class SimulationEngine extends NarratorObject {
 
-	public static String PROPERTY_TRANSITION_FIRED = "Transition fired";	
-	
+	public static String PROPERTY_TRANSITION_FIRED = "Transition fired";
+
+	public enum ResourceSelectionMode {
+		LIST, RANDOM
+	}
+
 	// engine config vars
-	public enum ResourceSelectionMode { LIST, RANDOM }
 	private final ResourceSelectionMode resourceSelectionMode;
 	private boolean respectSafetyRequirements;
-	
 	private final SimulationModel simulationModel;
-	private LinkedList<Place> currentMarking = new LinkedList<Place>();
+
+	// important vars while simulation
 	private HashMap<String, Transition> fireableTransitions = new HashMap<String, Transition>();
-	
+	private HashMap<Transition, Policy> policiesToSatisfy = new HashMap<Transition, Policy>();
+	private HashMap<Transition, SimulationEvent> internalEventMap = new HashMap<Transition, SimulationEvent>();
+
 	public SimulationEngine(SimulationModel simulationModel) {
-		this.simulationModel = simulationModel;
-		this.resourceSelectionMode = ResourceSelectionMode.RANDOM;
-		this.init();
+		this(simulationModel, ResourceSelectionMode.RANDOM);
 	}
 
 	public SimulationEngine(SimulationModel simulationModel, ResourceSelectionMode mode) {
 		this.simulationModel = simulationModel;
 		this.resourceSelectionMode = mode;
-		this.init();		
+		this.init();
 	}
-	
+
 	private void init() {
 		// should the engine respect safety requirements?
 		respectSafetyRequirements = true;
-		
+
 		// init fireable transitions
 		updateFireableTransitions();
 	}
 
-	private void updateFireableTransitions(){
-		HashMap<String, Transition> fireableTransitions = new HashMap<String, Transition>();	
+	private void updateFireableTransitions() {
+		HashMap<String, Transition> fireableTransitions = new HashMap<String, Transition>();
 		for (Transition transition : simulationModel.getNet().getTransitions()) {
-			if(transition.isFireable())
+			if (transition.isFireable())
 				fireableTransitions.put(transition.getId(), transition);
 		}
 		this.fireableTransitions = fireableTransitions;
 	}
-	
-	private Transition getRandomFireableTransition(){
+
+	private Transition getRandomFireableTransition() {
 		Random generator = new Random();
 		Object[] values = fireableTransitions.values().toArray();
 		return (Transition) values[generator.nextInt(values.length)];
 	}
 
 	public void run() {
-		while( !fireableTransitions.isEmpty() ) {
+		while (!fireableTransitions.isEmpty()) {
 			Transition transition = getRandomFireableTransition();
-			
+
 			// internal (not observable operations)
 			fire(transition);
 			Subject subject = firedby(transition);
-			
+
 			// generate event
-			if ( transition.getName() != null )
-				notifyListeners(this, PROPERTY_TRANSITION_FIRED , new SimulationEvent(transition, subject, transition.getUsedObject()));
+			SimulationEvent event = new SimulationEvent(transition, subject, transition.getUsedObject());
+			internalEventMap.put(transition, event);
+			
+			// the event is observable, if the transition has a label (no silent transition)
+			if (transition.getName() != null)
+				notifyListeners(this, PROPERTY_TRANSITION_FIRED, event);
 		}
-	}	
-	
-	private void fire(Transition transition) {		
+	}
+
+	private void fire(Transition transition) {
 		// we fire it we remove it
-		fireableTransitions.remove(transition.getId());	
+		fireableTransitions.remove(transition.getId());
 		// remove tokens form pre set
 		for (Arc arc : transition.getIncomingArcs()) {
-			Place p =  ((Place) arc.getSource());
+			Place p = ((Place) arc.getSource());
 			p.setMarking(p.getMarking() - 1);
 		}
 		// add tokens to post set
 		for (Arc arc : transition.getOutgoingArcs()) {
-			Place p =  ((Place) arc.getTarget());
+			Place p = ((Place) arc.getTarget());
 			p.setMarking(p.getMarking() + 1);
-		}		
+		}
 		// check what is now fireable
 		updateFireableTransitions();
 	}
@@ -96,14 +104,54 @@ public class SimulationEngine extends NarratorObject {
 		// get subjects which are authorized (implied through domain assignment)
 		HashSet<Subject> subjects = simulationModel.getResourceModel().getDomainFor(transition).getMembers();
 		
-		// get delegations and add subjects that are authorized through the delegations
-		if ( respectSafetyRequirements && simulationModel.getSafetyRequirements().hasDelegation(transition)) {
-			HashSet<Role> delegationRoles = simulationModel.getSafetyRequirements().getDelegations().get(transition);
-			for (Role role : delegationRoles) {
-				subjects.addAll(role.getMembers());
+		// check if safetyRquirements should be respected
+		if (respectSafetyRequirements) {
+			// get delegations and add subjects that are authorized through the
+			// delegations
+			if (simulationModel.getSafetyRequirements().hasDelegation(transition)) {
+				HashSet<Role> delegationRoles = simulationModel.getSafetyRequirements().getDelegations().get(transition);
+				for (Role role : delegationRoles) {
+					subjects.addAll(role.getMembers());
+				}
 			}
+
+			// check if transition is an eventually part of one or more policies
+			if (policiesToSatisfy.containsKey(transition)) {
+				Policy policy = policiesToSatisfy.get(transition);
+				SimulationEvent event = internalEventMap.get(policy.getObjective());
+				
+				// set the available subjects according to the policy rules
+				if( policy.getType() == PolicyType.SEPERATION_OF_DUTY ) {
+					subjects.remove(event.getSubject());
+				} else if ( policy.getType() == PolicyType.BINDING_OF_DUTY  ) {
+					subjects = new HashSet<Subject>();
+					subjects.add(event.getSubject());
+				} else if ( policy.getType() == PolicyType.CONFLICT_OF_INTEREST ) {
+					// remove subjects if they have a role, which is not a role of the subject that executed the objective task
+					for (Subject subject2 : subjects) {
+						if( !subject2.hasCompatibleRoles(event.getSubject()) ) {
+							subjects.remove(subject2);
+						}
+					}
+				} else if ( policy.getType() == PolicyType.USAGE_RESTRICTION ) {
+					
+				} else if ( policy.getType() == PolicyType.ACTION_REQUIREMENT ) {
+					
+				}
+			}
+
+			// check if transition is an objective of one or more policies
+			if (simulationModel.getSafetyRequirements().hasPolicy(transition)) {
+				HashSet<Policy> policies = simulationModel.getSafetyRequirements().getPolicyMap().get(transition);
+				// add policies to policiesToSatisfy map
+				for (Policy policy : policies) {
+					policiesToSatisfy.put(policy.getEventually(), policy);
+				}
+			}
+
 		}
-		
+
+		// resource selection
 		if (resourceSelectionMode == ResourceSelectionMode.LIST) {
 			subject = subjects.iterator().next();
 		} else {
@@ -114,5 +162,5 @@ public class SimulationEngine extends NarratorObject {
 		}
 		return subject;
 	}
-	
+
 }
