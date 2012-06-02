88f8012c3379626705648f2d517bdf222662eec5
Sebastian <sebastian.sebald@gmail.com>
2012-06-02 19:28:39 +0200

    added method getNonEventuallyTransitions

--- src/de/freiburg/uni/iig/sisi/model/ProcessModel.java
+++ src/de/freiburg/uni/iig/sisi/model/ProcessModel.java
@@ -1,12 +1,14 @@
 package de.freiburg.uni.iig.sisi.model;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
 import de.freiburg.uni.iig.sisi.model.net.PTNet;
+import de.freiburg.uni.iig.sisi.model.net.Transition;
 import de.freiburg.uni.iig.sisi.model.resource.ResourceModel;
 import de.freiburg.uni.iig.sisi.model.safetyrequirements.SafetyRequirements;
 import de.freiburg.uni.iig.sisi.utils.PNMLReader;
@@ -49,6 +51,13 @@ public class ProcessModel extends ModelObject {
 	public void createFromPNML(String uri) throws ParserConfigurationException, SAXException, IOException {
 		PNMLReader reader = new PNMLReader();
 		reader.createModelFromPNML(this, uri);
+	}
+	
+	public ArrayList<Transition> getNonEventuallyTransitions(){
+		ArrayList<Transition> transitions = getNet().getTransitions();
+		// remove transitions that are eventually part (silencing it would cause conflicts)	
+		transitions.removeAll(getSafetyRequirements().getEventuallyMap());
+		return transitions;
 	}
 	
 }
