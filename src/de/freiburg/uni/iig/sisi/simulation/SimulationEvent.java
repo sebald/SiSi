df91c75b2460f3e5f7213057f25fc239098cb1ac
Sebastian <sebastian.sebald@gmail.com>
2012-06-02 15:25:17 +0200

    removed timestamp b/c not used at all

--- src/de/freiburg/uni/iig/sisi/simulation/SimulationEvent.java
+++ src/de/freiburg/uni/iig/sisi/simulation/SimulationEvent.java
@@ -1,6 +1,5 @@
 package de.freiburg.uni.iig.sisi.simulation;
 
-import java.sql.Timestamp;
 import java.util.HashSet;
 
 import de.freiburg.uni.iig.sisi.model.net.Transition;
@@ -9,20 +8,22 @@ import de.freiburg.uni.iig.sisi.model.resource.WorkObject;
 
 public class SimulationEvent {
 	
+	private final String simulationID;
 	private final Transition transition;
 	private final Subject subject;
 	private final HashSet<WorkObject> usedObjects;
-	private final Timestamp timestamp;
 	
-	public SimulationEvent(Transition transition, Subject subject, HashSet<WorkObject> usedObjects) {
+	public SimulationEvent(String simulationID, Transition transition, Subject subject, HashSet<WorkObject> usedObjects) {
 		super();
 		
+		this.simulationID = simulationID;
 		this.transition = transition;
 		this.subject = subject;
 		this.usedObjects = usedObjects;
-		
-		java.util.Date date= new java.util.Date();
-		this.timestamp = new Timestamp(date.getTime());
+	}
+
+	public String getSimulationID() {
+		return simulationID;
 	}
 
 	public Transition getTransition() {
@@ -39,11 +40,11 @@ public class SimulationEvent {
 
 	@Override
 	public String toString() {
-		return "[" + this.transition.getName() + ", "+ this.subject.getName() + ", " + this.usedObjects + ", @" + this.timestamp + "]";
+		return "[" + this.getSimulationID() + ", " + this.transition.getName() + ", " + this.subject.getName() + ", " + this.usedObjects + "]";
 	}
 	
 	public String toCSV() {
-		return this.timestamp + ", " + this.transition.getName() + ", "+ this.subject.getName() + ", " + this.usedObjects;
+		return this.getSimulationID() + ", " + this.transition.getName() + ", "+ this.subject.getName() + ", " + this.usedObjects;
 	}
 
 }
