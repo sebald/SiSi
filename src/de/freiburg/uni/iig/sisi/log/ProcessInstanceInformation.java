314892fcad461ae70b494bf6f8e1d903d34f6372
Sebastian <sebastian.sebald@gmail.com>
2012-06-24 19:53:37 +0200

    better deviation information

--- src/de/freiburg/uni/iig/sisi/log/ProcessInstanceInformation.java
+++ src/de/freiburg/uni/iig/sisi/log/ProcessInstanceInformation.java
@@ -1,6 +1,9 @@
 package de.freiburg.uni.iig.sisi.log;
 
+import java.util.List;
+
 import de.freiburg.uni.iig.sisi.model.ModelObject;
+import de.freiburg.uni.iig.sisi.model.net.Node;
 import de.freiburg.uni.iig.sisi.model.net.variant.NetDeviation;
 import de.freiburg.uni.iig.sisi.model.net.variant.NetDeviation.DeviationType;
 
@@ -30,11 +33,32 @@ public class ProcessInstanceInformation extends ModelObject {
 	public String toString() {
 		String nl = System.lineSeparator();
 		
-		String s = "RunID: " + getId() + nl + "Deviation Type: " + deviationType;
-		if ( deviation != null ) {
-			s += nl + "Original Values: " + deviation.getOldValues() + nl;
-			s += "New Values: " + deviation.getNewValues();
-			
+		String s = "RunID: " + getId() + nl + "Deviation Type: " + deviationType + nl + "Details: ";
+		
+		switch (deviationType) {
+		case NONE:
+			s += "original model";
+			break;
+		case SKIPPING:
+			s += "Skipping Task \"" + deviation.getOldValues().get(0) + "\"";
+			break;
+		case SWAPPING:
+			s += "Position of Task \"" + deviation.getOldValues().get(0) + "\" has been swapped with Position of Task \"" + deviation.getOldValues().get(1) + "\"";
+			break;
+		case AND2XOR:
+			List<Node> firstPlaces = deviation.getOldValues().subList(0, deviation.getOldValues().size()/2);
+			List<Node> secondPlaces = deviation.getOldValues().subList((deviation.getOldValues().size()/2)+1, deviation.getOldValues().size());
+			s += "Merged " + firstPlaces + " to one place with ID " + deviation.getNewValues().get(0);
+			s += " and merged " + secondPlaces + " to one place with ID " + deviation.getNewValues().get(1);
+			break;
+		case XOR2AND:
+			List<Node> firstPlace = deviation.getNewValues().subList(0, deviation.getNewValues().size()/2);
+			List<Node> secondPlace = deviation.getNewValues().subList((deviation.getNewValues().size()/2)+1, deviation.getNewValues().size());
+			s += "Splitted " + deviation.getOldValues().get(0) + " to places with IDs " + firstPlace;
+			s += " and splitted " + deviation.getOldValues().get(1) + " to places with IDs " + secondPlace;
+			break;
+		default:
+			break;
 		}
 		
 		return s;
